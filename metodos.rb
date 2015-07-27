a56af0bebd546062a1eef8effdc2d2c003c064c7
Pablo FC <pablo.f.casal@openmailbox.org>
2015-07-27 14:30:07 +0200

    segundo archivo

---
+++ metodos.rb
@@ -0,0 +1,19 @@
+puts "Metodo sencillo de una suma"
+def primer_metodo
+   puts 2323 + 555
+   
+end
+
+primer_metodo
+puts "--------------------------------------"
+
+puts "Método sencillo con argumentos"
+def segundo a, b
+   puts (a + b)**4
+end
+
+segundo 45, 6
+segundo 1, -1
+puts "--------------------------------------"
+
+
